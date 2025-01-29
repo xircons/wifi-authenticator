f5484437c065d55a75bb4521731f4814243eef1f
WUTTIKAN <wutthikan_s@cmu.ac.th>
2025-01-29 21:27:46 +0700

    Update WifiLoginGUI.java

--- WifiLoginGUI.java
+++ WifiLoginGUI.java
@@ -1,9 +1,5 @@
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-
+import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
